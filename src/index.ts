65ab131c6c1e1ce358ac6163567ed62ec25f7617
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-09 17:23:12 +0300

    allow access to all initial routes and initiate db instance

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { client, connectToDatabase } from './db/connect';
+import { createGateway } from './views/createGateway';
+import { deleteGateway } from './views/deleteGateway';
+import { getAllGateways } from './views/getAllGateways';
+import { getGateway } from './views/getGateway';
+import { updateGateway } from './views/updateGateway';
+
+const app = express();
+app.use(express.json());
+
+
+async function startServer() {
+  await connectToDatabase();
+  app.post('/gateways', createGateway);
+  app.get('/gateways/:id', getGateway);
+  app.put('/gateways/:id', updateGateway);
+  app.delete('/gateways/:id', deleteGateway);
+  app.get('/gateways', getAllGateways);
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
+}
+
+async function stopServer() {
+  await disconnectFromDatabase();
+}
+async function disconnectFromDatabase() {
+    await client.close();
+}
+startServer();
