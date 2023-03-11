c8fd0df18b1e80c7e4554e0a9ee7c38833773d85
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:06:33 +0300

    ensure users can update a peripheral

---
+++ src/views/updatePeripheral.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { gatewaysCollection } from "../db/connect";
+import { peripheralDevice } from "../requestSchema/gateway";
+import { deviceDateCreated, deviceStatus } from "../utils/deviceHelpers";
+
+export async function updatePeripheral(req: Request, res: Response) {
+  const { id, uid } = req.params;
+  const { vendor, status, dateCreated } = req.body;
+  const filter = {
+    _id: new ObjectId(id),
+  };
+  const docId = Number(uid);
+  const filterExists = {
+    ...filter,
+    peripheralDevices: {
+      uid: { $ne: docId },
+      $or: [{ vendor }, { status }],
+    },
+  };
+  const otherExisting = await gatewaysCollection.findOne(filterExists);
+  if (otherExisting) {
+    return res.status(400).json({
+      message: "there is an existing device with the same vendor and status",
+    });
+  }
+  const udpateFilter = {
+    ...filter,
+    "peripheralDevices.uid": docId,
+  };
+  const updateGateway = {
+    $set: {
+      "peripheralDevices.$.vendor": vendor,
+      "peripheralDevices.$.status": deviceStatus(status),
+      "peripheralDevices.$.dateCreated": deviceDateCreated(dateCreated),
+    },
+  };
+  const result = await gatewaysCollection.updateOne(
+    udpateFilter,
+    updateGateway
+  );
+  if (result.modifiedCount === 0) {
+    return res.status(404).json({ message: "no new update" });
+  } else {
+    return res.status(204).json({ message: "updated" });
+  }
+}
