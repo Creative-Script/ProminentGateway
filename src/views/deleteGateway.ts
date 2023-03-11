cf7403245fa19617faf1e9ba36a335e87ece6d9b
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:08:33 +0300

    ensure proper deletion of Gateway

--- src/views/deleteGateway.ts
+++ src/views/deleteGateway.ts
@@ -5,8 +5,8 @@ export async function deleteGateway(req: Request, res: Response) {
   const { id } = req.params;
   const result = await gatewaysCollection.deleteOne({ _id: new ObjectId(id) });
   if (result.deletedCount === 0) {
-    res.sendStatus(404);
+    return res.sendStatus(404);
   } else {
-    res.sendStatus(204);
+    return res.sendStatus(204);
   }
 }
