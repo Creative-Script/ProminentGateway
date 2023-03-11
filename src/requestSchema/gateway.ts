71daac7a6126151fa89f01e9b5856d8155074608
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:13:40 +0300

    edit required and invalid fields

--- src/requestSchema/gateway.ts
+++ src/requestSchema/gateway.ts
@@ -4,13 +4,12 @@ import Joi from 'joi';
 
 export const peripheralDevice = Joi.object({
   uid: Joi.number().integer().min(1),
-  vendor: Joi.string().valid("online", "offline").default("online"),
-  status: Joi.string().required(),
+  vendor: Joi.string().required(),
+  status: Joi.string().valid("online", "offline").default("online").required(),
   dateCreated: Joi.date().default((new Date()).toISOString())
 });
 export const gatewaySchema = Joi.object({
-  name: Joi.string().required(),
   ipv4Address: Joi.string().ip().required(),
   serialNumber: Joi.string().required(),
-  peripheralDevices: Joi.array().items(peripheralDevice).default([])
+  peripheralDevices: Joi.array().items(peripheralDevice).max(10).default([])
 });
