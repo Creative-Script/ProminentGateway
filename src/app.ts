e6895deb49a1ee845f314d7bf114e089dc9b5bc0
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:10:32 +0300

    include routes for device manipulation

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,67 @@
-import express from 'express';
-import { client, connectToDatabase } from './db/connect';
-import swaggerUi from 'swagger-ui-express';
-import {swaggerDocument} from './swagger';
+import express, { NextFunction, Request, Response } from "express";
+import { client, connectToDatabase } from "./db/connect";
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocument } from "./swagger";
 
-import { createGateway } from './views/createGateway';
-import { deleteGateway } from './views/deleteGateway';
-import { getAllGateways } from './views/getAllGateways';
-import { getGateway } from './views/getGateway';
-import { updateGateway } from './views/updateGateway';
-import { validateGateway, validatePeripheralDevice } from './requestSchema/validator';
+import { createGateway } from "./views/createGateway";
+import { deleteGateway } from "./views/deleteGateway";
+import { getAllGateways } from "./views/getAllGateways";
+import { getGateway } from "./views/getGateway";
+import { updateGateway } from "./views/updateGateway";
+import {
+  validateGateway,
+  validatePeripheralDevice,
+} from "./requestSchema/validator";
+import { getAllPeripherals } from "./views/getAllPeripherals";
+import { addPeripheralDevice } from "./views/createPeripheral";
+import { deletePeripheral } from "./views/deletePeripheral";
+import { updatePeripheral } from "./views/updatePeripheral";
+import { getPeripheral } from "./views/getPeripheral";
 
+// start app
 const app = express();
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// setup swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// setup error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode).json({
+    message: err.message,
+    error: process.env.NODE_ENV === "production" ? {} : err.stack,
+  });
+});
 
 client;
+async function home(req: Request, res: Response) {
+  res.status(200).json({ time: new Date(), api: "prominent gateways api" });
+}
 async function startServer() {
   await connectToDatabase();
-  app.post('/gateways', validateGateway, createGateway);
-  app.get('/gateways/:id', getGateway);
-  app.put('/gateways/:id',validateGateway, updateGateway);
-  app.delete('/gateways/:id', deleteGateway);
-  app.get('/gateways', getAllGateways);
-  app.get('/gateways/:id/peripheralDevices', );
-  app.post('/gateways/:id/peripheralDevices',validatePeripheralDevice);
-  app.put('/gateways/:id/peripheralDevices/:uid',validatePeripheralDevice,);
-  app.delete('/gateways/:id/peripheralDevices/:uid');
-  var listener = app.listen(3000,() => {
-    console.log(`Server started on port ${listener.address()['port']}`);
+  // register routes
+  app.get("/", home);
+  app.post("/gateways", validateGateway, createGateway);
+  app.get("/gateways/:id", getGateway);
+  app.put("/gateways/:id", validateGateway, updateGateway);
+  app.delete("/gateways/:id", deleteGateway);
+  app.get("/gateways", getAllGateways);
+  app.get("/gateways/:id/peripheralDevices", getAllPeripherals);
+  app.post(
+    "/gateways/:id/peripheralDevices",
+    validatePeripheralDevice,
+    addPeripheralDevice
+  );
+  app.put(
+    "/gateways/:id/peripheralDevices/:uid",
+    validatePeripheralDevice,
+    updatePeripheral
+  );
+  app.delete("/gateways/:id/peripheralDevices/:uid", deletePeripheral);
+  app.get("/gateways/:id/peripheralDevices/:uid", getPeripheral);
+  // expose port
+  var listener = app.listen(3000, () => {
+    console.log(`Server started on port ${listener.address()["port"]}`);
   });
 }
 
@@ -35,7 +69,7 @@ async function stopServer() {
   await disconnectFromDatabase();
 }
 async function disconnectFromDatabase() {
-    await client.close();
+  await client.close();
 }
 
-export { app,startServer, stopServer };
+export { app, startServer, stopServer };
