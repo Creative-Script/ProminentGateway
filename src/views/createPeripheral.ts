9399ce77286371297e63ff6a0f05ad859b80d87f
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:04:24 +0300

    ensure users can add peripherals to gateways

---
+++ src/views/createPeripheral.ts
@@ -0,0 +1,60 @@
+import { ObjectId } from "mongodb";
+import { Request, Response } from "express";
+import { gatewaysCollection } from "../db/connect";
+import { Gateway, PeripheralDevice } from "../interfaces/gateway";
+import { newDeviceCreation } from "../utils/deviceHelpers";
+
+export async function addPeripheralDevice(req: Request, res: Response) {
+  const { id } = req.params;
+  const { vendor, dateCreated, status } = req.body;
+  const filter = { _id: new ObjectId(id) };
+  const projection = {
+    peripheralDevices: 1,
+    serialNumber: 1,
+  };
+  const gateway = await gatewaysCollection.findOne(filter, { projection });
+
+  if (!gateway) {
+    return res.status(404).json({ message: "Gateway not found" });
+  }
+
+  if (gateway.peripheralDevices && gateway.peripheralDevices.length >= 10) {
+    return res
+      .status(400)
+      .json({ message: "Maximum number of peripheral devices reached" });
+  } else if (
+    gateway.peripheralDevices &&
+    gateway.peripheralDevices.filter(
+      (device) => device.vendor === vendor && device.status === status
+    ).length > 0
+  ) {
+    return res.status(400).json({ message: "Device already exists" });
+  }
+  const newDevice = await createDevice(gateway, vendor, dateCreated, status);
+
+  const updates = {
+    $push: { peripheralDevices: newDevice },
+  };
+  await gatewaysCollection.updateOne(filter, updates);
+  return res.send(newDevice);
+}
+
+async function createDevice(
+  gateway: Gateway,
+  vendor: string,
+  dateCreated: Date,
+  status: string
+): Promise<PeripheralDevice> {
+  
+  const devices = await gatewaysCollection.aggregate(
+    [
+      { $unwind: "$peripheralDevices" },
+      { $group: { _id: new ObjectId(gateway._id), maxUid: { $max: "$peripheralDevices.uid" } } },
+      { $project: { _id: 0, maxUid: 1 } },
+    ]
+  ).toArray()
+  const highestUid = devices.length>0?devices[0].maxUid:0;
+  const newUid = highestUid + 1;
+
+  return newDeviceCreation(newUid, vendor, dateCreated, status);
+}
