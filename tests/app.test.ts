73755d2f413a0e4a7f6e9db61459d932cdcfc41f
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2023-03-12 00:15:37 +0300

    ensure collection is emptied after tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,21 +1,22 @@
-import './base-test'
+import "./base-test";
 import { app, startServer, stopServer } from "../src/app";
 import request from "supertest";
+import { emptyDatabase } from "../src/db/connect";
 
 describe("API endpoint", () => {
-
-beforeAll(async () => {
-  // perform any setup required before starting the server
-  // e.g. connect to the database
-  await startServer();
-  // start the server and store a reference to the server object
-});
-afterAll(async () => {
-  // perform any setup required before starting the server
-  // e.g. connect to the database
-  await stopServer();
-  // start the server and store a reference to the server object
-});
+  beforeAll(async () => {
+    // perform any setup required before starting the server
+    // e.g. connect to the database
+    await startServer();
+    // start the server and store a reference to the server object
+  });
+  afterAll(async () => {
+    // perform any setup required before starting the server
+    // e.g. connect to the database
+    await emptyDatabase();
+    await stopServer();
+    // start the server and store a reference to the server object
+  });
 
   it("should return a 200 status code and a JSON object", async () => {
     const res = await request(app).get("/gateways");
